75fa05287faa2ca13bd2a414c25699fa3a024504
James Immer <77898354+jimurrito@users.noreply.github.com>
2022-05-08 00:02:12 -0700

    Upgraded to 1.03.50722
    BIG UPDATE
    Most of the app was refactored. Major improvements to optimization, and processing.
    
    > Added Classes Job and Packer, with respective methods
    > New Idle Function, for waiting for workorders
    > Replaced DocPrep() with DocMod()
    > Dedicated Functions for repeated actions, JobFail(), JobVal()
    > removed SelectMatrix(), Packer() (Labheim), VMBuilder
    > Dedicated packer module
    > Packer Module no longer relies on pipes or managed temporary files
    > Fixed Optimization issue on idle
    > Added KVM Mount Only option for debugging
    > Added Extra Love

--- labheim.py
+++ labheim.py
@@ -1,126 +1,268 @@
 from datetime import datetime
 import os
 import re
+from enum import Enum
+from time import sleep
+import libvirt
 
-def DocPrep(
-    Type: int = 99,
-    Host: str = "VMDly",
-    Name: str = "Administrator",
-    Pwd: str = "Password1",
-    DiskPath: str = "",
-    ISOPATH: str = "",
-    ISOSUM: str = "",
-    Src: str = "templates/autounattend.xml",
+
+class Job:
+    # OS Options
+    class OSName(Enum):
+        Ws19 = "Windows-Server 19"
+        Ws22 = "Windows-Server 22"
+        W10e = "Windows 10"
+        UbuS = "Ubuntu-Server"
+        UbuD = "Ubuntu-Desktop"
+
+    # ISO RE Patterns
+    class ISOName(Enum):
+        Ws19 = "winserver_2019\.[a-zA-Z]+"
+        Ws22 = "winserver_2022\.[a-zA-Z]+"
+        W10e = "windows10_21h2_ENT\.[a-zA-Z]+"
+        UbuS = "ubuntu_server_21\.10_amd64\.[a-zA-Z]+"
+        UbuD = "ubuntu_desktop_21\.04_amd64\.[a-zA-Z]+"
+
+    # Role Options
+    class RoleName(Enum):
+        VAN = "Vanilla"
+        DJ = "Domain Joined"
+        DC = "Domain Controller"
+
+    # Creates Job Obj
+    def __init__(self, Job):
+        self.Host = Job["VMName"]
+        self.Name = Job["Credentials"]["Username"]
+        self.Pwd = Job["Credentials"]["Password"]
+        self.Path = f"/kvm/{self.Host}"
+        self.Os = int(Job["OS"])
+        self.Role = int(Job["Role"])
+        self.AS = bool(Job["AutoStart"])
+        self.CPU = int(Job["vCPU"])
+        self.RAM = int(Job["RAM"])
+        self.Net = Job["Network"]
+
+    # Gets OS Name from Int
+    def getOSName(self):
+        """Gets OS Name for Verbose Output
+        \nOS Int --> OS Str
+        """
+        if self.Os == 0:
+            return self.OSName.Ws19.value
+        elif self.Os == 1:
+            return self.OSName.Ws22.value
+        elif self.Os == 2:
+            return self.OSName.W10e.value
+        elif self.Os == 3:
+            return self.OSName.UbuS.value
+        elif self.Os == 4:
+            return self.OSName.UbuD.value
+        else:
+            raise Exception("Invalid OS Int Provided")
+
+    # Gets Role Name from Int
+    def getRoleName(self):
+        """Gets Role Name for Verbose Output
+        \nRole Int --> Role Str
+        """
+        if self.Role == 0:
+            return self.RoleName.VAN.value
+        elif self.Role == 1:
+            return self.RoleName.DJ.value
+        elif self.Role == 2:
+            return self.RoleName.DC.value
+        else:
+            raise Exception("Invalid Role Int Provided")
+
+    # Gets ISO Path
+    def getISO(self, ISOPath: str):
+        """Gets .ISO file Path, and Associated .Hash File
+        \n >> ISO Path
+        \n >> ISO MD5 Hash
+        """
+        if self.Os == 0:
+            Name = self.ISOName.Ws19.value
+        elif self.Os == 1:
+            Name = self.ISOName.Ws22.value
+        elif self.Os == 2:
+            Name = self.ISOName.W10e.value
+        elif self.Os == 3:
+            Name = self.ISOName.UbuS.value
+        elif self.Os == 4:
+            Name = self.ISOName.UbuD.value
+        else:
+            raise Exception("Invalid OS Int Provided")
+        Iso = list()
+        for i in os.listdir(ISOPath):
+            try:
+                Iso.append(re.search(Name, i).group(0))
+            except:
+                pass
+        if not Iso:
+            raise Exception(f"No .ISOs or .HASH Found in '{ISOPath}'")
+        ISOfile = f"{ISOPath}{Iso[0]}"
+        Hash = open(f"{ISOPath}{Iso[1]}", "r").read()
+        return ISOfile, Hash
+
+    def ValJob(self, Type: int, VHead="CONFIG", Log=...):
+        """Validates if Domain already exists.
+        Checks if Domain is Defined in KVM, or files exist in the Target Path.
+        """
+        # QEMU State
+        Qemu = libvirt.open("qemu:///system")
+        # KVM Eval
+        try:
+            (Qemu.lookupByName(self.Host)).isActive()
+            kvm = True
+        except:
+            kvm = False
+        # File Eval
+        try:
+            os.listdir(self.Path)
+            file = True
+        except:
+            file = False
+
+        class Error(Enum):
+            PreKVM = ".X0F100"
+            PreFile = ".X0F100"
+            PostKVM = ".X0F001"
+            PostFile = ".X0F002"
+
+        # ValMatrix 0 == Pre; 1 == Post
+        # KVM Val
+        if Type == 0 and kvm == True or Type == 1 and kvm == False:
+            if Type == 0:
+                e = Error.PreKVM.value
+            elif Type == 1:
+                e = Error.PostKVM.value
+            VLog(
+                Header="FAILURE",
+                MSG=f"KVM Validation Failure [{e}] for Domain '{self.Host}'. Ending Job... ",
+                DIR=Log,
+            )
+            return e
+        else:
+            VLog(
+                Header=VHead,
+                MSG=f"KVM Validation Successful for Domain '{self.Host}'.",
+                DIR=Log,
+            )
+        # File Val
+        if Type == 0 and file == True or Type == 1 and file == False:
+            if Type == 0:
+                e = Error.PreFile.value
+            elif Type == 1:
+                e = Error.PostFile.value
+            VLog(
+                Header="FAILURE",
+                MSG=f"File Validation Failure [{e}] for Domain '{self.Host}'. Ending Job ",
+                DIR=Log,
+            )
+            return e
+        else:
+            VLog(
+                Header=VHead,
+                MSG=f"File Validation Successful for Domain '{self.Host}'.",
+                DIR=Log,
+            )
+            return
+
+    def DefineBuild(self):
+        """Defines Packer Built Domain, as a KVM Domain"""
+        VHead = "KVM"
+        Log = f"logs/{self.Host}_KVM.log"
+        build = open("templates/virt-install-win.temp", "r").read()
+        VLog(Header=VHead, MSG="Generating KVM|QEMU Domain", DIR=Log)
+        if self.AS == True:
+            As = "--autostart"
+        else:
+            As = ""
+        build = (
+            build.replace("$HOST$", self.Host)
+            .replace("$vCPU$", str(self.CPU))
+            .replace("$RAM$", str(self.RAM))
+            .replace("$Net$", self.Net)
+            .replace("$DiskPath$", self.Path)
+            .replace("$AStrt$", As)
+        )
+        os.system(f"{build} > {Log}")
+
+
+def DocMod(
+    job: Job,
+    File: str,
+    RootPath: str = os.getcwd(),
+    ISOPath: str = ...,
+    ISOHash: str = ...,
 ):
-    doc = (open(Src, "r")).read()
-    if Type != 99:
-        if Type == 0:
-            FLC = open("templates/FLC-Vanilla.xml", "r").read()
-        if Type == 1:
-            FLC = open("templates/FLC-DJ.xml", "r").read()
-        doc = doc.replace("$FLC$", FLC)
-    doc = doc.replace("$HOST$", Host)
-    doc = doc.replace("$UserName$", Name)
-    doc = doc.replace("$Password$", Pwd)
-    doc = doc.replace("$DiskPath$", DiskPath)
-    doc = doc.replace("$osPath$", ISOPATH)
-    doc = doc.replace("$osSum$", ISOSUM)
-    return doc
+    """Pipes Input into Template Files.
+    \nCan Consume Class Job Objects
+    """
+
+    class FLC(Enum):
+        Vanilla = "FLC-Vanilla"
+        DJ = "FLC-DJ"
+
+    if not isinstance(job, Job):
+        raise Exception("Input Object is not Class Job")
+    if File.find("$FLC$") != -1:
+        if job.Role == 0:
+            Cmd = open(f"{RootPath}/templates/{FLC.Vanilla.value}.xml", "r").read()
+        elif job.Role == 1:
+            Cmd = open(f"{RootPath}/templates/{FLC.DJ.value}.xml", "r").read()
+        else:
+            raise Exception("Input Role Not Defined")
+        File = File.replace("$FLC$", Cmd)
+    if ISOPath != Ellipsis:
+        File = File.replace("$osPath$", ISOPath)
+    if ISOHash != Ellipsis:
+        File = File.replace("$osSum$", ISOHash)
+    return (
+        File.replace("$HOST$", job.Host)
+        .replace("$UserName$", job.Name)
+        .replace("$Password$", job.Pwd)
+        .replace("$DiskPath$", job.Path)
+    )
+
+
+def JobWait(Dir: str = "WorkOrders/", VHead: str = "IDLE", Log: str = ...):
+    if Log == Ellipsis:
+        raise Exception("Log Directory Argument Required")
+    VLog(Header=VHead, MSG=(f"Waiting for New WorkOrders"), DIR=Log)
+    n = 0
+    while True:
+        WorkOrders = os.listdir(Dir)
+        if WorkOrders:
+            for Order in WorkOrders:
+                try:
+                    re.search(".json", Order).group(0)
+                except:
+                    WorkOrders.remove(Order)
+            if WorkOrders:
+                return WorkOrders
+        sleep(30)
+        n += 1
+        # 30m heart-beat
+        if n == 900:
+            VLog(Header=VHead, MSG=(f"Waiting loop Heart-beat"), DIR=Log)
+            n = 0
+
+
+def JobFail(Error: str, Path: str, Root: str = "WorkOrders/"):
+    os.rename(
+        f"{Root}{Path}",
+        f"{Root}finished/" + Path.replace(".json", Error),
+    )
 
 
 def VLog(
     Header: str = "Header",
     MSG: str = "Message",
-    DIR: str = 'log',
+    DIR: str = "log",
 ):
     Time = (datetime.now()).isoformat(timespec="seconds")
     Output = f"[{Time}]-[{Header}]-[{MSG}]"
     (open(f"./{DIR}", "a")).write("\n")
     (open(f"./{DIR}", "a")).write(Output)
     print(Output)
-
-
-def SelectMatrix(Type: int = 0, IN: int = 0, ROOT: str = "/cd_disk/KVM/iso/"):
-    if Type == 0:
-        if IN == 0:
-            a = "Windows-Server 19"
-            b = f"{ROOT}winserver_2019.iso"
-        if IN == 1:
-            a = "Windows-Server 22"
-            b = f"{ROOT}winserver_2022.iso"
-        if IN == 2:
-            a = "Windows 10"
-            b = f"{ROOT}windows10_21h2_ENT.iso"
-        if IN == 3:
-            a = "Ubuntu-Server"
-            b = f"{ROOT}ubuntu_server_21.10_amd64.iso"
-        if IN == 4:
-            a = "Ubuntu-Desktop"
-            b = f"{ROOT}ubuntu_desktop_21.04_amd64.iso"
-        c = open(f"{b.replace('.iso','.hash')}", "r").read()
-        return a, b, c
-
-    if Type == 1:
-        if IN == 0:
-            a = "Vanilla"
-        if IN == 1:
-            a = "Domain Joined"
-        if IN == 2:
-            a = "Domain Controller"
-        return a
-
-
-def Packer(cmd: str, Path: str = "./", Plog: str = "PackerLog"):
-    Tlog = "tlog"
-    if cmd == "build":
-        os.system(f"PACKER_LOG=1 packer build {Path} | tee {Plog} {Tlog}")
-        while True:
-            try:
-                re.search("Error", open(Tlog, "r").read()).group(0)
-                try:
-                    re.search("permission denied", open(Tlog, "r").read()).group(0)
-                    c = 2
-                    break
-                except:
-                    c = 1
-                    break
-            except:
-                pass
-            try:
-                re.search(
-                    "Builds finished\. The artifacts of successful builds are:",
-                    open(Tlog, "r").read(),
-                ).group(0)
-                c = 0
-                break
-            except:
-                pass
-        os.remove(Tlog)
-    else:
-        os.system(f"packer {cmd} {Path} | tee {Plog} {Tlog} ")
-        try:
-            re.search("Error", open(Tlog, "r").read()).group(0)
-            c = 1
-        except:
-            c = 0
-        os.remove(Tlog)
-    return c
-
-
-def VMBuilder(
-    AStrt: bool, Host: str, vCPU: str, RAM: str, Net: str, DiskPath: str, Log: str
-):
-    build = open("templates/virt-install-win.temp", "r").read()
-    vCPU = str(vCPU)
-    RAM = str(RAM)
-    if AStrt == True:
-        AStrt = "--autostart"
-    else:
-        AStrt = ""
-    build = build.replace("$Host$", Host)
-    build = build.replace("$vCPU$", vCPU)
-    build = build.replace("$RAM$", RAM)
-    build = build.replace("$Net$", Net)
-    build = build.replace("$DiskPath$", DiskPath)
-    build = build.replace("$AStrt$", AStrt)
-    os.system(f"{build} > {Log}")
--- main.py
+++ main.py
@@ -7,17 +7,16 @@
 # [Modules]
 import os
 import json
-from time import sleep
-import re
-import libvirt
-import labheim
+from labheim import Job, JobWait, VLog, DocMod, JobFail
+from packer import Packer
 
 
 # [Parameters]
 #
-Verbose = True
-MountOnly = False
 GenLog = "logs/VMD.log"
+RootPath = os.getcwd()
+Path_to_ISO = "/cd_disk/KVM/iso/"
+MountOnly = False
 
 
 def main():
@@ -26,246 +25,131 @@ def main():
     (open(f"./{GenLog}", "w")).write((open("templates/logo", "r")).read())
 
     Header = "PREP"
-    labheim.VLog(Header="Start-Up", MSG="Start-Up", DIR=GenLog)
+    VLog("Start-Up", "Start-Up", GenLog)
     # Main Loop
     while True:
 
         # Loop Waiting for WorkOrders
-        Header = "IDLE"
-        labheim.VLog(Header=Header, MSG=(f"Waiting for New WorkOrders"), DIR=GenLog)
-        n = 0
-        while True:
-            WorkOrders = os.listdir("WorkOrders/")
-            if WorkOrders:
-                for Order in WorkOrders:
-                    try:
-                        re.search(".json", Order).group(0)
-                    except:
-                        WorkOrders.remove(Order)
-                if WorkOrders:
-                    break
-            else:
-                sleep(30)
-                n += 1
-                # 30m heart-beat
-                if n == 900:
-                    labheim.VLog(
-                        Header=Header, MSG=(f"Waiting loop Heart-beat"), DIR=GenLog
-                    )
-                    n = 0
+        WorkOrders = JobWait(Log=GenLog)
+
         # Count
         Cb = 0
         Ca = sum(1 for c in WorkOrders if c.endswith(".json"))
-        # Foreach Job
+        # Foreach Order
         for JobPath in WorkOrders:
             Header = "CONFIG"
             Cb += 1
-            labheim.VLog(Header=Header, MSG=(f"Working on Job ({Cb}/{Ca})"), DIR=GenLog)
+            VLog(Header, (f"Working on WorkOrder ({Cb}/{Ca})"), GenLog)
 
             # [Injest WorkOrder]
-            # Grabs job from folder, loads it into json format
-            Job = json.loads(open(f"WorkOrders/{JobPath}", "r").read())
-            labheim.VLog(
-                Header=Header, MSG="Injesting and Parsing WorkOrders", DIR=GenLog
-            )
-            Host = Job["VMName"]
-            Name = Job["Credentials"]["Username"]
-            Pwd = Job["Credentials"]["Password"]
-            Path = f"/kvm/{Host}"
+            # Grabs Order from folder, loads it into json format
+            VLog(Header, "Injesting and Parsing WorkOrders", GenLog)
+            Order = Job(json.loads(open(f"WorkOrders/{JobPath}", "r").read()))
 
-            # [Pre-Validation]
-            # QEMU State
-            Qemu = libvirt.open("qemu:///system")
-            try:
-                if (Qemu.lookupByName(Host)).isActive():
-                    labheim.VLog(
-                        Header="FAILURE",
-                        MSG=f"Domain '{Host}' already Exists. Ending Job [X0F010]",
-                        DIR=GenLog,
-                    )
-                    PreVal = True
-                    ErNum = ".X0F010"
-            except:
-                labheim.VLog(
-                    Header=Header,
-                    MSG=(f"Domain '{Host}' not currently defined."),
-                    DIR=GenLog,
-                )
-                try:
-                    os.listdir(f"/kvm/{Host}")
-                    labheim.VLog(
-                        Header="FAILURE",
-                        MSG=f"Files for Domain '{Host}' already Exist. Ending Job [X0F020]",
-                        DIR=GenLog,
-                    )
-                    PreVal = True
-                    ErNum = ".X0F020"
-                except:
-                    labheim.VLog(
-                        Header=Header,
-                        MSG=f"Files for Domain '{Host}' not Found. Building Domain.",
-                        DIR=GenLog,
-                    )
-            # PreValidation Filter
-            try:
-                if PreVal:
-                    NewPath = JobPath.replace(".json", ErNum)
-                    if JobPath.endswith(".json"):
-                        os.rename(
-                            f"WorkOrders/{JobPath}", f"WorkOrders/finished/{NewPath}"
-                        )
+            if MountOnly != True:
+
+                # Domain Validation
+                Error = Order.ValJob(0, Log=GenLog)
+                if Error:
+                    JobFail(Error, JobPath)
                     break
-            except:
-                labheim.VLog(
-                    Header=Header,
-                    MSG=f"PreValidation Successful, Starting Build for '{Host}'",
-                    DIR=GenLog,
+                else:
+                    VLog(
+                        Header,
+                        f"PreValidation Successful, Starting Build for '{Order.Host}'",
+                        GenLog,
+                    )
+                Error = 0
+
+                # [OS-Selection]
+                VLog(
+                    Header,
+                    f"Domain '{Order.Host}' OS will be '{Order.getOSName()}'",
+                    GenLog,
+                )
+                # [Role-Selection]
+                VLog(
+                    Header,
+                    f"Domain '{Order.Host}' Role will be '{Order.getRoleName()}'",
+                    GenLog,
                 )
 
-            # [OS-Selection]
-            # WS19      == 0
-            # WS21      == 1
-            # W10       == 2
-            # Ubntu-Svr == 3
-            # Ubntu-Dsk == 4
-            Os, OSPath, OSSum = labheim.SelectMatrix(Type=0, IN=int(Job["OS"]))
-            labheim.VLog(
-                Header=Header, MSG=f"Domain '{Host}' OS will be '{Os}'", DIR=GenLog
-            )
+                # Gets ISOPath and MD5-Hash
+                ISOPath, Hash = Order.getISO(Path_to_ISO)
 
-            # [Role-Selection]
-            # Vanilla   == 0
-            # DJ        == 1
-            # DC        == 2
-            Role = labheim.SelectMatrix(Type=1, IN=int(Job["Role"]))
-            labheim.VLog(
-                Header=Header, MSG=f"Domain '{Host}' Role will be '{Role}'", DIR=GenLog
-            )
-
-            # [DOCMOD]
-            # Inject Workorder Params into autounattend.xml
-            (open("./answer_files/autounattend.xml", "w")).write(
-                labheim.DocPrep(Type=int(Job["Role"]), Host=Host, Name=Name, Pwd=Pwd)
-            )
-            labheim.VLog(
-                Header=Header, MSG="New AutoUnattend.xml Generated", DIR=GenLog
-            )
-            # Inject WorkOrder Params into vars.auto.pkrvars.hcl
-            (open("./vars.auto.pkrvars.hcl", "w")).write(
-                labheim.DocPrep(
-                    Host=Host,
-                    Name=Name,
-                    Pwd=Pwd,
-                    DiskPath=Path,
-                    ISOPATH=OSPath,
-                    ISOSUM=OSSum,
-                    Src=("templates/vars.pkrvars.hcl"),
+                # [DOCMOD]
+                # Autounattend.xml
+                (open("./answer_files/autounattend.xml", "w")).write(
+                    DocMod(Order, (open("templates/autounattend.xml", "r").read()))
                 )
-            )
-            labheim.VLog(
-                Header=Header, MSG="New Packer Variable File Generated", DIR=GenLog
-            )
-            # Inject WorkOrder Params into WinRM Config Script
-            (open("Post-build/scripts/winRM_config_enable.ps1", "w")).write(
-                labheim.DocPrep(Name=Name, Pwd=Pwd, Src=("templates/winRM.temp.ps1"))
-            )
-            labheim.VLog(
-                Header=Header, MSG="New WinRM Configuration File Generated", DIR=GenLog
-            )
+                VLog(Header, "New AutoUnattend File Generated", GenLog)
+                # vars.auto.pkrvars.hcl
+                (open("./vars.auto.pkrvars.hcl", "w")).write(
+                    DocMod(
+                        Order,
+                        (open("templates/vars.pkrvars.hcl", "r").read()),
+                        ISOPath=ISOPath,
+                        ISOHash=Hash,
+                    )
+                )
+                VLog(Header, "New Packer Variable File Generated", GenLog)
+                # winRM_config_enable.ps1
+                (open("Post-build/scripts/winRM_config_enable.ps1", "w")).write(
+                    DocMod(Order, (open("templates/winRM.temp.ps1", "r").read()))
+                )
+                VLog(Header, "New WinRM Configuration File Generated", GenLog)
 
-            # [PACKER]
-            if MountOnly == False:
+                # [PACKER]
                 Header = "PACKER"
-                PakLog = f"logs/{Host}_Packer.log"
-                # Init.
-                if labheim.Packer(cmd="init", Plog=PakLog) == 0:
-                    labheim.VLog(Header=Header, MSG="Packer Initilized", DIR=GenLog)
-                else:
-                    labheim.VLog(
-                        Header=Header, MSG="Packer Initilization Failed", DIR=GenLog
+                Build = Packer(Order, GenLog=GenLog)
+                try:
+                    Build.Init()
+                except:
+                    Error = ".X0F010"
+                try:
+                    Build.Format()
+                except:
+                    Error = ".X0F020"
+                try:
+                    Build.Validate()
+                except:
+                    Error = ".X0F030"
+                try:
+                    Build.Build()
+                except:
+                    Error = ".X0F040"
+
+                # Packer Fail Catch
+                if not isinstance(Error, int):
+                    VLog(
+                        "FAILURE",
+                        f"Packer Execution Failure [{Error}] for Domain '{Order.Host}'. Ending Job... ",
+                        GenLog,
                     )
-                    PFail = True
-                # Fmt.
-                if labheim.Packer(cmd="fmt", Plog=PakLog) == 0:
-                    labheim.VLog(Header=Header, MSG="Packer Files Formated", DIR=GenLog)
-                else:
-                    labheim.VLog(
-                        Header=Header, MSG="Packer Files Failed to Format", DIR=GenLog
-                    )
-                    PFail = True
-                # Validate
-                if labheim.Packer(cmd="validate", Plog=PakLog) == 0:
-                    labheim.VLog(
-                        Header=Header, MSG="Packer Files Validated", DIR=GenLog
-                    )
-                else:
-                    labheim.VLog(
-                        Header=Header,
-                        MSG="Packer Files Failed to Validated",
-                        DIR=GenLog,
-                    )
-                    PFail = True
-                # Build
-                p = labheim.Packer(cmd="build", Plog=PakLog)
-                if p == 0:
-                    labheim.VLog(
-                        Header=Header, MSG="Packer Build Completed", DIR=GenLog
-                    )
-                elif p == 2:
-                    labheim.VLog(
-                        Header=Header,
-                        MSG="Packer Build Lacks Required Premissions",
-                        DIR=GenLog,
-                    )
-                    PFail = True
-                else:
-                    labheim.VLog(Header=Header, MSG="Packer Build Failed", DIR=GenLog)
-                    PFail = True
-            # Packer Fail Catch
-            if PFail:
-                exit(
-                    "Critical Error. Packer Failed to Build. Check Packer Logs for More Information. VMD Exiting"
-                )
+                    JobFail(Error, JobPath)
+                    break
 
             # [KVM]
             # Installs New Image as a KVM Domain
-            Header = "KVM"
-            KVMLog = f"logs/{Host}_KVM.log"
-            labheim.VLog(Header=Header, MSG="Generating KVM|QEMU Domain", DIR=GenLog)
-            labheim.VMBuilder(
-                AStrt=bool(Job["AutoStart"]),
-                Host=Host,
-                vCPU=(Job["vCPU"]),
-                RAM=(Job["RAM"]),
-                Net=(Job["Network"]),
-                DiskPath=(f"{Path}/vdisk0"),
-                Log=KVMLog,
-            )
+            Order.DefineBuild()
+
             # Validating Domain was Set
-            try:
-                if (Qemu.lookupByName(Host)).isActive():
-                    labheim.VLog(
-                        Header=Header, MSG="Domain Successfully Deployed", DIR=GenLog
-                    )
-
-                    NewPath = JobPath.replace(".json", ".X0")
-                    if JobPath.endswith(".json"):
-                        os.rename(
-                            f"WorkOrders/{JobPath}", f"WorkOrders/finished/{NewPath}"
-                        )
-            except:
-                labheim.VLog(
-                    Header=Header,
-                    MSG="Domain Failed to be Deployed [X0F001] ",
-                    DIR=GenLog,
-                )
-                Path = Job.replace(".json", ".X0F001")
-                os.rename(f"WorkOrders/{Job}", f"WorkOrders/finished/{Path}")
+            Error = Order.ValJob(1, Log=GenLog)
+            if Error:
+                JobFail(Error, JobPath)
                 break
-        else:
-            continue
+            else:
+                VLog(
+                    Header,
+                    f"Domain '{Order.Host}' Successfully Deployed.",
+                    GenLog,
+                )
+                os.rename(
+                    f"WorkOrders/{JobPath}",
+                    f"WorkOrders/finished/" + JobPath.replace(".json", ".X0"),
+                )
+                break
 
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-main()
+if __name__ == "__main__":
+    main()
